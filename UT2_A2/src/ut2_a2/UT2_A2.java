79064f9e373c1be9d374639bf5ad5ca9e5f2249c
Eduardo <Eduardo@192.168.1.39>
2015-10-06 15:16:29 +0100

    Primeros archivos subidos a github con netbeans

---
+++ UT2_A2/src/ut2_a2/UT2_A2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut2_a2;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class UT2_A2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal ventana = new VentanaPrincipal();
+      //  ventana.setVisible(true);
+    }
+    
+}
