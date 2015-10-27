81620fa405d9abf17317a205f6128e36ca227e9d
Eduardo <Eduardo@192.168.1.37>
2015-10-27 15:49:03 +0000

    Creacion JavaBeans reloj. Ya funciona todo!!!

---
+++ PruebaBean/src/pruebabean/PruebaBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebabean;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class PruebaBean {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setVisible(true);
+    }
+    
+}
