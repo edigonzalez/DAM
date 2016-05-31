dc62a46442df36644d2fd0445fcd709558b85917
Eduardo Díaz González <diazgonzalezeduardo@gmail.com>
2016-05-31 01:57:38 +0100

    Implementacion de Javadoc en ccuenta.java

--- src/dam_ed04_actividad/CCuenta.java
+++ src/dam_ed04_actividad/CCuenta.java
@@ -7,49 +7,79 @@ package dam_ed04_actividad;
 
 
 public class CCuenta {
-
-
+    
+    
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
-
+    /**
+     * Constructor sin parámetros
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+    * Constructor al que se le pasan los parámetros siguientes: 
+    * @param nom Nombre del Cliente
+    * @param cue Número de Cuenta
+    * @param sal Saldo de la cuenta
+    * @param tipo Tipo de Interes
+    * @author Pruebas
+    */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
         cuenta=cue;
         saldo=sal;
     }
-
+    
+    /**
+     * Método para asignar un nombre de cliente al objeto CCCuenta
+     * @param nom String: Nombre del Cliente
+     */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
-
+    
+    /**
+     * Método que devuelve el nombre del cliente
+     * @return String: Nombre del cliente
+     */
     public String obtenerNombre()
     {
         return getNombre();
     }
 
-
+    /**
+     * Método que devuelve el saldo de la cuenta
+     * @return double: cantidad de saldo
+     */
     public double estado()
     {
         return getSaldo();
     }
-
-
+    
+    /**
+     * Método para ingresar saldo en la cuenta
+     * @param cantidad double: cantidad a ingresar
+     * @throws Exception No se puede ingresra cantidades negativas
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    
+    /**
+     * Método para retirar saldo en la cuenta
+     * @param cantidad double: cantidad a retirar
+     * @throws Exception No se puede retirar cantidades negativas
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -59,17 +89,22 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
-
+    /**
+     * Método que nos devuelve el número de cuenta
+     * @return String: Número de cuenta
+     */
     public String obtenerCuenta()
     {
         return getCuenta();
     }
 
-
+    /**
+     * Método que nos devuelve el nombre del cliente
+     * @return String: Nombre del cliente
+     */
     public String getNombre() {
         return nombre;
     }
-
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
